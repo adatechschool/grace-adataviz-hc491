54c2612cbe56efd404a4b3676cf04e712f5fa91f
Chau <hoaichau20@hotmail.com>
2025-12-02 13:56:13 +0100

    titre+image

--- src/main.js
+++ src/main.js
@@ -1,28 +1,26 @@
-
 async function fetchApi() {
   try {
     const response = await fetch(
-      "https://opendata.paris.fr/explore/embed/dataset/arbresremarquablesparis/table/?basemap=jawg.dark&location=12,48.85653,2.34892&dataChart=eyJxdWVyaWVzIjpbeyJjaGFydHMiOlt7InR5cGUiOiJsaW5lIiwiZnVuYyI6IkFWRyIsInlBeGlzIjoiYXJicmVzX2lkYmFzZSIsInNjaWVudGlmaWNEaXNwbGF5Ijp0cnVlLCJjb2xvciI6IiMyNjM4OTIifV0sInhBeGlzIjoiZ2VucmUiLCJtYXhwb2ludHMiOiIiLCJ0aW1lc2NhbGUiOm51bGwsInNvcnQiOiIiLCJjb25maWciOnsiZGF0YXNldCI6ImFyYnJlc3JlbWFycXVhYmxlc3BhcmlzIiwib3B0aW9ucyI6e319fV0sImRpc3BsYXlMZWdlbmQiOnRydWUsImFsaWduTW9udGgiOnRydWUsInRpbWVzY2FsZSI6IiJ9"
+      "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/arbresremarquablesparis/records?limit=20"
     );
     const dataReturn = await response.json();
     console.log(dataReturn);
 
     const div = document.getElementById("app");
-    for (let i=0; i < dataReturn.results.length; i++) {
-      const liste = document.createElement("li"); 
-      liste.innerHTML = `<h1>${dataReturn.results[i].arbres_libellefrancais}</h1><h3>${dataReturn.results[i].arbres_espece}</h3>`
+    for (let i = 0; i < dataReturn.results.length; i++) {
+      const liste = document.createElement("li");
+      liste.innerHTML = `
+      <h1>${dataReturn.results[i].arbres_libellefrancais}</h1>
+      <h3>${dataReturn.results[i].com_annee_plantation}</h3>
+      <h2>${dataReturn.results[i].com_resume}</h2>
+      <p>${dataReturn.results[i].com_descriptif}</p>
+      <img src="${dataReturn.results[i].com_url_photo1}" alt="Photo arbre remarquable" style="width:300px; height:auto;">`;
       document.body.appendChild(liste);
-    
     }
-
-
-    /* return dataReturn */;
+    /* return dataReturn; */
   } catch (error) {
     console.log(error);
   }
 }
 
 fetchApi();
-
-
-

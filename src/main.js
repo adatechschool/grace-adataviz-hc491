6acc6c219d338040ceeed0a42fe2221bff090940
Chau <hoaichau20@hotmail.com>
2025-12-10 12:20:29 +0100

    version final 1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
+//Déclare une fonction asynchrone nommée fetchApi
 async function fetchApi() {
-  //Déclare une fonction asynchrone nommée fetchApi
   try {
+    //va chercher des données sur internet et attend qu'elles arrivent avt de continuer
     const response = await fetch(
-      // on attend la réponse réseau avant de continuer
       "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/arbresremarquablesparis/records?limit=20"
     );
     const dataReturn = await response.json(); //lit la réponse et le convertit en objet JS décode le JSON/await car response.json() retourne une promesse
@@ -10,54 +10,60 @@ async function fetchApi() {
 
     const div = document.getElementById("app"); // récupère dans la page l'élement HTML qui a id=app
 
-    const container = document.createElement("div"); //création du container pour l'input barre de recherche
+    //création du CONTAINER principal
+    const container = document.createElement("div");
     container.id = "container";
     div.appendChild(container);
 
+    //création du CONTAINER pour la barre de recherche + suggestions
     const searchContainer = document.createElement("div");
     searchContainer.id = "search-container";
     container.appendChild(searchContainer);
 
+    //création de la barre de recherche
     const searchBar = document.createElement("input");
     searchBar.type = "text";
     searchBar.id = "search-bar";
     searchBar.placeholder = "Rechercher un type d'arbre...";
     /* searchContainer.appendChild(searchBar); */
 
+    //création d'une div pour afficher les suggestions d'autocomplétion
     const suggestionsBox = document.createElement("div");
     suggestionsBox.id = "suggestions";
     /* searchContainer.appendChild(suggestionsBox); */
 
-    //création du wrapper pour l'input et les suggestions
+    //création du wrapper pour englober l'input+ suggestions
     const inputWrapper = document.createElement("div");
     inputWrapper.id = "input-wrapper";
     inputWrapper.appendChild(searchBar);
     inputWrapper.appendChild(suggestionsBox);
     searchContainer.appendChild(inputWrapper);
 
-    const searchButton = document.createElement("button"); // création du bouton "rechercher"
+    // création du bouton 'rechercher'
+    const searchButton = document.createElement("button");
     searchButton.textContent = "Rechercher";
     searchContainer.appendChild(searchButton);
 
+    // création d'une div pour afficher les résultats de la recherche(cartes d'arbres)
     const resultsContainer = document.createElement("div");
     resultsContainer.id = "results";
     container.appendChild(resultsContainer);
 
-    // suggestions quand on tape
+    // suggestions quand on tape ds l'input
     searchBar.addEventListener("input", () => {
       const term = searchBar.value.toLowerCase();
       suggestionsBox.innerHTML = ""; // vide le conteneur avant de le remplir a nouveau
-      if (term.length < 1) return; //n'affiche pas trop tôt
+      if (term.length < 1) return;
       const suggestions = dataReturn.results.filter(
         (
-          arbre // on filtre sur nom et adresse
+          arbre // on filtre sur nom et adresse seulement
         ) =>
           arbre.arbres_libellefrancais.toLowerCase().includes(term) ||
           arbre.arbres_adresse.toLowerCase().includes(term)
       );
 
+      // affiche 8 suggestions au max
       suggestions.slice(0, 8).forEach((arbre) => {
-        // on garde 8 suggestions max
         const option = document.createElement("div");
         option.classList.add("suggest-item");
 
@@ -67,7 +73,6 @@ async function fetchApi() {
         option.addEventListener("click", () => {
           searchBar.value = arbre.arbres_libellefrancais;
           suggestionsBox.innerHTML = "";
-
           displayResults([arbre]); // affiche seulement la fiche sélectionnée
         });
 
@@ -79,7 +84,6 @@ async function fetchApi() {
     function displayResults(results) {
       resultsContainer.innerHTML = ""; //vide les résultats précédents
       for (let i = 0; i < results.length; i++) {
-        //boucle pour parcourir l'API
         const liste = document.createElement("li"); // crée un élément <li> en mémoire ( pas encore inséré dans le DOM)
         liste.classList.add("card");
 
@@ -101,11 +105,9 @@ async function fetchApi() {
         bouton.addEventListener("click", () => {
           // qd on clique, on vérifie l'état d'affichage de 'description' et on bascule (block <-> none) et on met à jour le texte du bouton 'voir plus' 'voir moins'
           if (description.style.display === "none") {
-            // vérifie si le style inline 'display' vaut 'none'
             description.style.display = "block"; //rend visible la description
             bouton.textContent = "Voir Moins"; // change le libellé du bouton
           } else {
-            // sinon
             description.style.display = "none"; // cache de nouveau
             bouton.textContent = "Voir Plus"; // et remet le texte initial
           }
@@ -132,8 +134,8 @@ async function fetchApi() {
     /* return dataReturn; */
   } catch (error) {
     // si une erreur survient dans le try ( ex fetch echoue), on la capture et on l'affiche dans la console
-    console.log(error); //
+    console.log(error);
   }
 }
 
-fetchApi(); // appel de la fonction
+fetchApi();
